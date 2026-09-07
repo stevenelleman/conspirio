83951801b184143cb9b034c61950f52d12b200e8
Steven Elleman <steven.elleman@gmail.com>
2026-09-07 15:08:06 -0700

    show communities by default until community-issued chips offered

--- apps/frontend/src/pages/community/index.tsx
+++ apps/frontend/src/pages/community/index.tsx
@@ -88,7 +88,8 @@ const CommunityPage = () => {
     );
   }
 
-  if (userChips.length === 0) {
+  // Until Embassy chips are added, allow Embassy to be seen by default
+  /*if (userChips.length === 0) {
     return (
       <div className="flex min-h-screen justify-center items-center text-center px-4">
         <span className="text-label-secondary">
@@ -96,7 +97,7 @@ const CommunityPage = () => {
         </span>
       </div>
     );
-  }
+  }*/
 
   return (
     <>
@@ -116,7 +117,9 @@ const CommunityPage = () => {
                   }}
                 ></div>
               </div>
-              <div className="py-3 flex gap-6">
+              {/*
+               // NOTE: Until community-issued chips are offered, show existing communities
+               <div className="py-3 flex gap-6">
                 {userChips && userChips.includes(ChipIssuer.ETH_INDIA_2024) && (
                   <NavTab
                     active={activeTab === ActiveTab.ETHINDIA}
@@ -148,6 +151,40 @@ const CommunityPage = () => {
                       Edge Lanna
                     </NavTab>
                   )}
+              </div>*/}
+              <div className="py-3 flex gap-6">
+                <NavTab
+                  active={activeTab === ActiveTab.ETHINDIA}
+                  onClick={() => {
+                    setActiveTab(ActiveTab.ETHINDIA);
+                  }}
+                >
+                  EthIndia
+                </NavTab>
+                <NavTab
+                  active={activeTab === ActiveTab.DEVCON}
+                  onClick={() => {
+                    setActiveTab(ActiveTab.DEVCON);
+                  }}
+                >
+                  Devcon
+                </NavTab>
+                <NavTab
+                  active={activeTab === ActiveTab.LANNA}
+                  onClick={() => {
+                    setActiveTab(ActiveTab.LANNA);
+                  }}
+                >
+                  Edge Lanna
+                </NavTab>
+                <NavTab
+                  active={activeTab === ActiveTab.EMBASSY}
+                  onClick={() => {
+                    setActiveTab(ActiveTab.EMBASSY);
+                  }}
+                >
+                  Embassy
+                </NavTab>
               </div>
             </div>
           )
