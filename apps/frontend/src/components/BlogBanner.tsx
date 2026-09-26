76e91bfee2c94afc59b1a1388855b89a798416d1
Steven Elleman <steven.elleman@gmail.com>
2026-09-25 18:32:17 -0700

    add untracked files

---
+++ apps/frontend/src/components/BlogBanner.tsx
@@ -0,0 +1,45 @@
+import useSettings from "@/hooks/useSettings";
+import { cn } from "@/lib/frontend/util";
+import Image from "next/image";
+import { SUPPORT_CONTACT } from "@/constants";
+import Link from "next/link";
+
+export const BlogBanner = () => {
+  const { darkTheme } = useSettings();
+  return (
+    <Link href={SUPPORT_CONTACT}>
+      <div className="flex flex-col gap-1">
+        <span className="text-base font-bold font-sans text-label-primary">
+          Conspirio blog - coming soon
+        </span>
+        <div
+          className={cn(
+            "w-full rounded-lg border border-primary ",
+            darkTheme ? "!border !border-white bg-card-gray" : "bg-white"
+          )}
+        >
+          <div className="p-2 flex items-center gap-[10px]">
+            <div className="flex-shrink-0">
+              <Image
+                src="/images/build.png"
+                alt="shop cover"
+                width={80}
+                height={80}
+                className="w-20 h-20 rounded-lg object-cover"
+              />
+            </div>
+            <div className="flex flex-col flex-1">
+              {/* <p className="text-xs font-bold text-label-quaternary">47 taps</p> */}
+              <h2 className="text-sm font-bold text-label-primary">
+                Writing in progress!
+              </h2>
+              <p className="text-xs font-medium text-label-quaternary">
+                Featuring essays on the design, engineering, intentions, and inspiration behind Conspirio.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
